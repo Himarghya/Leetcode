5d09b84c5b31e5aecc88576785e043b18ea5ab83
Himarghya Das <himarghyadas@gmail.com>
2026-07-18 01:07:00 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.62%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0 ; int end = nums.size() - 1;
+        while(start <= end){
+            int middle = start + ((end - start)/2);
+            if(nums[middle] == target){
+                return middle;
+            }
+            if(nums[middle] > target){
+                end = middle -1;
+            }else{
+                start = middle +1;
+            }
+        }
+        return -1;
+        
+    }
+};
