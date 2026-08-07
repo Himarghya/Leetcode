a5adf3d4b9322f4ffb55793a194dba91243ed5d2
Himarghya Das <himarghyadas@gmail.com>
2026-08-07 19:42:56 +0530

    Time: 0 ms (100%), Space: 23.7 MB (50.76%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& arry) {
-        int freq1 =0 ;int count1 = 0;
-        int freq2 = 1 ; int count2 =0;
-       for(int i =0; i < arry.size() ; i++){
-         if(arry[i] == freq1){
-            count1++;
-         }else if(arry[i] == freq2){
-            count2++;
-         }
-         else if(count1 == 0){
-            freq1 = arry[i];
-            count1 = 1;
-         }else if(count2 == 0){
-            freq2 = arry[i];
-            count2 = 1;
-         }else{
-            count1 --;
-            count2--;
-         }
+        vector<int> ans;
+    int count1 = 0; int count2 = 0;
+    int freq1 = 0; int freq2 = 0;
+    for(int i = 0; i < arry.size(); i++){
+        if( count1 == arry[i]){
+             freq1++;
+        }else if( count2 == arry[i]){
+            freq2++;
+        }else if(freq1 == 0){
+             count1 = arry[i];
+             freq1 = 1;
+        }else if(freq2 == 0){
+            count2 = arry[i];
+            freq2 = 1;
+            
+        }else{
+            freq1--; freq2--;
         }
-       count1 = 0 ; 
-       count2 = 0;
-      for(int i = 0 ; i < arry.size() ; i++){
-        if(arry[i] == freq1 ){
-            count1++;
-        }else if(arry[i] == freq2){
-            count2++;
-        }
-      }
-    vector<int> ans;
-    if(count1 > arry.size()/3){
-        ans.push_back(freq1);
     }
-     if(count2 > arry.size()/3){
-        ans.push_back(freq2);
+    int c1 = 0; int c2 = 0;
+    for(int i = 0; i < arry.size(); i++){
+        if(count1 == arry[i]){
+            c1++;
+        }else if(count2 == arry[i]){
+            c2++;
+        }
+    }
+    if(c1 > (arry.size() / 3)){
+        ans.push_back(count1);
+    }
+    if(c2 > (arry.size() / 3)){
+        ans.push_back(count2);
+    }
+    for(int i =0 ; i< ans.size() ; i++){
+        cout << ans[i] << " ";
     }
        return ans; 
     }
